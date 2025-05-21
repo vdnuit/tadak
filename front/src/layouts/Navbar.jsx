d11ab51aad75c193b88fa93bfefdc109e0599f2b
vdnuit <skush@g.skku.edu>
2025-05-21 20:56:28 +0900

    set some folders

---
+++ front/src/layouts/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <h2>로고</h2>
+      <ul>
+        <li>홈</li>
+        <li>소개</li>
+        <li>문의</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ front/src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <main>
+      <h1>타닥에 오신 것을 환영합니다</h1>
+      <p>이곳은 편지를 주고받는 공간입니다.</p>
+    </main>
+  );
+}
+
+export default Home;

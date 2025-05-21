dae17b08279983c7160d129da8de49d57991c73b
vdnuit <skush@g.skku.edu>
2025-05-21 16:29:32 +0900

    S+R: REST API test

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')  // Spring Boot에 요청
+      .then(res => res.text())  // 문자열로 응답 받음
+      .then(data => setMessage(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          잘 나오나아
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Spring에서 온 메시지:</p>
+        <p>{message}</p>
       </header>
     </div>
   );
